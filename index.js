b83b6ec2cc6b56ea2152cda8026bd8111af3a0a3
Peter Ramsing <peter.ramsing@naturalpoint.com>
2016-10-20 23:37:07 -0700

    Counts how many results for each area were tabulated

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ module.exports = {
   surveyTools: {
     totalResults: function(surveyResults) {
       return surveyResults.length;
+    },
+    tabulateResults: function(resultsArray, humanReadable) {
+      let counts = {};
+
+      for (let i = 0; i < resultsArray.length; i++) {
+        let resultArray = resultsArray[i];
+
+        for (let i = 0; i < resultArray.length; i++) {
+          result = resultArray[i];
+
+          if (counts.hasOwnProperty(result)) {
+            counts[result]++;
+          } else {
+            if (humanReadable) {
+              counts[result] = 1;
+            } else {
+              counts[result] = 0;
+            }
+          }
+        }
+      }
+      return counts;
     }
   }
 }
--- test/check.js
+++ test/check.js
@@ -47,3 +47,31 @@ describe('an array of survey results should return an object with each tabulated
     expect(totalResultsFunction).to.be.a('number');
   })
 });
+
+describe('return a count of each type of result', function() {
+  let trf = scheduler.surveyTools.tabulateResults(exampleSurveyResults);
+
+  it('should count how many people like one thing', function() {
+    expect(trf).to.have.deep.property('skiing', 2);
+    expect(trf).to.have.deep.property('hiking', 1);
+    expect(trf).to.have.deep.property('cycling', 4);
+    expect(trf).to.have.deep.property('reading', 2);
+    expect(trf).to.have.deep.property('cooking', 0);
+    expect(trf).to.have.deep.property('running', 2);
+    expect(trf).to.have.deep.property('other', 1);
+    expect(trf).to.have.deep.property('rowing', 1);
+  });
+
+  let trfRedable = scheduler.surveyTools.tabulateResults(exampleSurveyResults, true);
+  it('should return human readable numbers if asked for', function() {
+    expect(trfRedable).to.have.deep.property('skiing', 3);
+    expect(trfRedable).to.have.deep.property('hiking', 2);
+    expect(trfRedable).to.have.deep.property('cycling', 5);
+    expect(trfRedable).to.have.deep.property('reading', 3);
+    expect(trfRedable).to.have.deep.property('cooking', 1);
+    expect(trfRedable).to.have.deep.property('running', 3);
+    expect(trfRedable).to.have.deep.property('other', 2);
+    expect(trfRedable).to.have.deep.property('rowing', 2);
+  });
+
+});
