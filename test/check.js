fcb90bd4f712c5e41b4d47033f7b6f61ceb9d9b7
Peter Ramsing <peter.ramsing@naturalpoint.com>
2016-10-20 22:29:21 -0700

    Adds example data

--- test/check.js
+++ test/check.js
@@ -1,4 +1,26 @@
+const exampleSurveyResults = [
+  [ 'skiing', 'hiking', 'cycling' ],
+  [ 'reading', 'cooking', 'cycling' ],
+  [ 'running', 'reading', 'other' ],
+  [ 'rowing', 'running', 'cycling' ],
+  [ 'skiing', 'running', 'rowing' ],
+  [ 'other', 'hiking', 'cycling' ],
+  [ 'reading', 'skiing', 'cycling']
+];
 
+const exampleSurveyResultsTabulates = {
+  totalResults: 7,
+  individualResults : {
+    skiing: 3,
+    hiking: 2,
+    cycling: 5,
+    reading: 3,
+    cooking: 1,
+    running: 3,
+    other: 2,
+    rowing: 2
+  }
+}
 
 let expect    = require('chai').expect;
 
